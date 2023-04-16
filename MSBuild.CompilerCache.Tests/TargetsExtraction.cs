bd856d3a2778dedc1c615eb401ddfc884f7db063
janusz <janusz.wrobel.2@gmail.com>
2023-04-16 18:45:38 +0100

    WIP

--- MSBuild.CompilerCache.Tests/TargetsExtraction.cs
+++ MSBuild.CompilerCache.Tests/TargetsExtraction.cs
@@ -58,7 +58,123 @@ public class TargetsExtraction
         Utils.RunProcess("dotnet", "msbuild /pp:targets.xml", dir.Dir);
         File.Copy(Path.Combine(dir.Dir.FullName, "targets.xml"), outputPath, overwrite: true);
     }
-    
+
+    private static readonly string[] UseCacheConditions =
+    {
+        "('$(OutputType)' == 'Library' OR '$(OutputType)' == 'Exe')",
+        "'$(AdditionalLibPaths)' == ''",
+        "'@(AddModules)' == ''",
+        "'$(EmitCompilerGeneratedFiles)' != 'true'",
+        "'$(ProvideCommandLineArgs)' != 'true'",
+        "'$(ReportAnalyzer)' != 'true'",
+        "'$(SkipCompilerExecution)' != 'true'",
+        "'$(SourceLink)' == ''",
+        "'$(PublicSign)' != 'true'"
+    };
+
+    public enum AttrType
+    {
+        SimpleProperty,
+        Unsupported,
+        InputFiles,
+        OutputFile,
+        References,
+        Sources,
+        Ignore,
+        PathMap
+    }
+
+    public record Attr(string Name, AttrType Type);
+
+    public static Attr Unsup(string Name) => new Attr(Name, AttrType.Unsupported);
+    public static Attr Prop(string Name) => new Attr(Name, AttrType.SimpleProperty);
+    public static Attr InputFiles(string Name) => new Attr(Name, AttrType.InputFiles);
+    public static Attr OutputFile(string Name) => new Attr(Name, AttrType.OutputFile);
+    public static Attr Ignore(string Name) => new Attr(Name, AttrType.Ignore);
+
+    public static readonly Attr[] Attrs =
+    {
+        Unsup("AdditionalLibPaths"),
+        Unsup("AddModules"),
+        Unsup("AdditionalFiles"),
+        Prop("AllowUnsafeBlocks"),
+        Unsup("AnalyzerConfigFiles"),
+        Unsup("Analyzers"),
+        InputFiles("ApplicationConfiguration"),
+        Prop("BaseAddress"),
+        Prop("CheckForOverflowUnderflow"),
+        Prop("ChecksumAlgorithm"),
+        Prop("CodeAnalysisRuleSet"),
+        Prop("CodePage"),
+        Prop("DebugType"), // TODO Affects whether separate PDB files are generated - but PDB path is not populated when no separate pdb file used.
+        Prop("DefineConstants"),
+        Unsup("DelaySign"),
+        Prop("Deterministic"),
+        Prop("DisabledWarnings"),
+        Prop("DisableSdkPath"),
+        OutputFile("DocumentationFile"),
+        Prop("EmbedAllSources"),
+        InputFiles("EmbeddedFiles"),
+        Prop("EmitDebugInformation"),
+        Prop("EnvironmentVariables"),
+        Prop("ErrorEndLocation"),
+        Unsup("ErrorLog"),
+        Prop("ErrorReport"),
+        Prop("Features"),
+        Prop("FileAlignment"),
+        Unsup("GeneratedFilesOutputPath"),
+        Prop("GenerateFullPaths"),
+        Prop("HighEntropyVA"),
+        Prop("Instrument"),
+        Prop("KeyContainer"),
+        InputFiles("KeyFile"),
+        Prop("LangVersion"),
+        Unsup("LinkResources"),
+        Prop("MainEntryPoint"),
+        Unsup("ModuleAssemblyName"),
+        Prop("NoConfig"),
+        Prop("NoLogo"),
+        Prop("NoStandardLib"),
+        Prop("NoWin32Manifest"),
+        Prop("Nullable"),
+        Prop("Optimize"),
+        OutputFile("OutputAssembly"),
+        OutputFile("OutputRefAssembly"),
+        OutputFile("PdbFile"),
+        Prop("Platform"),
+        Prop("Prefer32Bit"),
+        Prop("PreferredUILang"),
+        Prop("ProvideCommandLineArgs"),
+        Prop("PublicSign"),
+        new Attr("References", AttrType.References),
+        Prop("RefOnly"),
+        Prop("ReportAnalyzer"),
+        InputFiles("Resources"),
+        Unsup("ResponseFiles"),
+        Prop("RuntimeMetadataVersion"),
+        Unsup("SharedCompilationId"),
+        Prop("SkipAnalyzers"),
+        Prop("SkipCompilerExecution"),
+        Prop("SourceLink"),
+        new Attr("Sources", AttrType.Sources),
+        Prop("SubsystemVersion"),
+        Prop("TargetType"),
+        Unsup("ToolExe"),
+        Unsup("ToolPath"),
+        Prop("TreatWarningsAsErrors"),
+        Prop("UseHostCompilerIfAvailable"),
+        Prop("UseSharedCompilation"),
+        Prop("Utf8Output"),
+        Unsup("VsSessionGuid"),
+        Prop("WarningLevel"),
+        Prop("WarningsAsErrors"),
+        Prop("WarningsNotAsErrors"),
+        InputFiles("Win32Icon"),
+        InputFiles("Win32Manifest"),
+        InputFiles("Win32Resource"),
+        new Attr("PathMap", AttrType.PathMap),
+    };
+
     [Test]
     public void Extract()
     {
@@ -68,14 +184,16 @@ public class TargetsExtraction
         };
         foreach (var sdk in sdks)
         {
-            foreach (var lang in new[] { SupportedLanguage.CSharp, SupportedLanguage.FSharp })
+            foreach (var lang in new[] { SupportedLanguage.CSharp /*, SupportedLanguage.FSharp */ })
             {
                 var allTargetsPath = $"Targets.{sdk}.{lang}.xml";
                 //GenerateAllTargets(sdk, lang, allTargetsPath);
                 var coreCompilePath = $"CoreCompile.{sdk}.{lang}.targets";
+                var cachedPath = $"Cached.CoreCompile.{sdk}.{lang}.targets";
                 var allTargets = XDocument.Load(allTargetsPath);
-                var xName = XName.Get("Target", "http://schemas.microsoft.com/developer/msbuild/2003");
-                var allTargetsList = allTargets.Root.Descendants(xName);
+                string nmsp = "http://schemas.microsoft.com/developer/msbuild/2003";
+                XName Name(string localName) => XName.Get(localName, nmsp);
+                var allTargetsList = allTargets.Root!.Descendants(Name("Target"));
                 var coreCompileTargetNode = allTargetsList
                     .Single(n => n.Attribute("Name")?.Value == "CoreCompile");
                 var root = allTargets.Root;
@@ -85,17 +203,143 @@ public class TargetsExtraction
                     if (e != coreCompileTargetNode)
                     {
                         e.Remove();
-                        Console.WriteLine($"Remove node");
                     }
                 }
 
                 {
-                    using var writer = XmlWriter.Create(coreCompilePath, new XmlWriterSettings { Indent = true, NewLineOnAttributes = true});
+                    using var writer = XmlWriter.Create(coreCompilePath,
+                        new XmlWriterSettings { Indent = true, NewLineOnAttributes = true });
                     allTargets.Save(writer);
                 }
                 var xml = File.ReadAllText(coreCompilePath);
                 xml = xml.Replace("&#xD;&#xA;", Environment.NewLine);
                 File.WriteAllText(coreCompilePath, xml);
+
+                var compilationTaskName =
+                    lang == SupportedLanguage.CSharp
+                        ? "Csc"
+                        : "Fsc";
+                var compilationTask = allTargets.Root.Descendants(Name(compilationTaskName)).Single();
+                var condition = compilationTask.Attribute("Condition");
+                if (condition == null)
+                {
+                    throw new NotSupportedException("Expected compilation task Condition attribute to be set.");
+                }
+
+                condition.Value = $"'$(DoInvokeCompilation)' == 'true' AND {condition.Value}";
+
+                var relevantAttributes =
+                    compilationTask.Attributes()
+                        .Where(a => a.Name.LocalName != "Condition" && !a.IsNamespaceDeclaration)
+                        .Select(a => (a.Name.LocalName, a.Value,
+                            KnownAttr: Attrs.FirstOrDefault(x => x.Name == a.Name.LocalName)))
+                        .ToImmutableArray();
+
+                var unknownAttributes =
+                    relevantAttributes
+                        .Where(x => x.KnownAttr == null)
+                        .ToImmutableArray();
+                if (unknownAttributes.Length > 0)
+                {
+                    throw new Exception($"{unknownAttributes.Length} unknown attributes found:" +
+                                        $"{string.Join(",", unknownAttributes.Select(a => a.LocalName))}");
+                }
+
+                relevantAttributes = relevantAttributes
+                    .Where(x => x.KnownAttr != null)
+                    .ToImmutableArray();
+
+                var startComment = new XComment("START OF CACHING EXTENSION CODE");
+                compilationTask.AddBeforeSelf(startComment);
+
+                var extraCanCacheConditions =
+                    relevantAttributes
+                        .Where(a => a.KnownAttr!.Type == AttrType.Unsupported)
+                        .Select(a => $"'{a.Value}' == ''")
+                        .ToImmutableArray();
+
+                var fullCanCacheCondition = UseCacheConditions.Union(extraCanCacheConditions).StringsJoin($"{Environment.NewLine}AND{Environment.NewLine}");
+                var propertygroup = Name("PropertyGroup");
+                var firstPropsGroupElement = new XElement(propertygroup);
+                var canCacheElement =
+                    new XElement(Name("CanCache"), new XAttribute("Condition", fullCanCacheCondition), "true");
+                firstPropsGroupElement.Add(canCacheElement);
+                compilationTask.AddBeforeSelf(firstPropsGroupElement);
+
+                var propsString =
+                    relevantAttributes
+                        .Where(x => new[] { AttrType.SimpleProperty, AttrType.OutputFile }.Contains(x.KnownAttr!.Type))
+                        .Select(x => $"{x.LocalName}={x.Value}")
+                        .StringsJoin(";");
+
+                var canCacheCondition = new XAttribute("Condition", "'$(CanCache)' == 'true'");
+                var propsGroupElement = new XElement(propertygroup, canCacheCondition);
+                var propsElement = new XElement(Name("PropertyInputs"), propsString);
+                propsGroupElement.Add(propsElement);
+                compilationTask.AddBeforeSelf(propsGroupElement);
+
+                var itemgroup = Name("ItemGroup");
+                var itemGroupElement = new XElement(itemgroup, canCacheCondition);
+                var inputFiles =
+                    relevantAttributes
+                        .Where(x => x.KnownAttr!.Type == AttrType.InputFiles)
+                        .ToImmutableArray();
+                // Add metadata with item names, similar to property names above, to avoid hash clashes.
+                var inputFileItems =
+                    inputFiles
+                        .Select(i => new XElement(Name("FileInputs"), new XAttribute("Include", i.Value)))
+                        .ToImmutableArray();
+                itemGroupElement.Add(inputFileItems);
+                compilationTask.AddBeforeSelf(itemGroupElement);
+
+                var locateElement = new XElement(Name("LocateCompilationCacheEntry"),
+                    canCacheCondition,
+                    new XAttribute("FileInputs", "@(FileInputs)"),
+                    new XAttribute("PropertyInputs", "@(PropertyInputs)"),
+                    new XAttribute("BaseCacheDir", "$(CompilationCacheBaseDir)"),
+                    new XElement(Name("Output"), new XAttribute("TaskParameter", "CacheDir"),
+                        new XAttribute("PropertyName", "CacheDir")),
+                    new XElement(Name("Output"), new XAttribute("TaskParameter", "CacheHit"),
+                        new XAttribute("PropertyName", "CacheHit"))
+                );
+                compilationTask.AddBeforeSelf(locateElement);
+
+                var gElement = new XElement(propertygroup,
+                    new XElement(Name("DoInvokeCompilation"),
+                        new XAttribute("Condition",
+                            "'$(CacheHit)' != 'true' OR '$(CompileAndCheckAgainstCache)' == 'true'"), "true"));
+                compilationTask.AddBeforeSelf(gElement);
+                var endComment = new XComment("END OF CACHING EXTENSION CODE");
+                compilationTask.AddBeforeSelf(endComment);
+
+                var outputsGroup = new XElement(itemgroup, canCacheCondition);
+                var outputItems =
+                    relevantAttributes
+                        .Where(x => x.KnownAttr!.Type == AttrType.OutputFile)
+                        .Select(x => new XElement(Name("CompileOutputsToCache"), new XAttribute("Include", x.Value)))
+                        .ToImmutableArray();
+                outputsGroup.Add(outputItems);
+
+                var useOrPopulateCacheElement =
+                    new XElement(Name("UseOrPopulateCache"),
+                        canCacheCondition,
+                        new XAttribute("IntermediateOutputPath", "$(IntermediateOutputPath)"),
+                        new XAttribute("OutputsToCache", "@(CompileOutputsToCache)"),
+                        new XAttribute("CheckCompileOutputAgainstCache", "$(CompileAndCheckAgainstCache)"),
+                        new XAttribute("CacheHit", "$(CacheHit)"),
+                        new XAttribute("CacheDir", "$(CacheDir)")
+                    );
+
+                compilationTask.AddAfterSelf(startComment, outputsGroup, useOrPopulateCacheElement, endComment);
+
+                {
+                    using var writer = XmlWriter.Create(cachedPath,
+                        new XmlWriterSettings { Indent = true, NewLineOnAttributes = true });
+                    allTargets.Save(writer);
+                }
+                xml = File.ReadAllText(cachedPath);
+                xml = xml.Replace("&#xD;&#xA;", Environment.NewLine);
+                File.WriteAllText(cachedPath, xml);
             }
         }
 
