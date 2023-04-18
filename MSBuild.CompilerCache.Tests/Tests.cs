7936d8327da1c6cea67240845a5ad1116a3e375e
janusz <janusz.wrobel.2@gmail.com>
2023-04-18 18:30:45 +0100

    F

--- MSBuild.CompilerCache.Tests/Tests.cs
+++ MSBuild.CompilerCache.Tests/Tests.cs
@@ -68,8 +68,12 @@ public sealed class BuildEnvironment : IDisposable
 
     private static string NuGetVersion()
     {
+        Console.WriteLine($"ThisAssembly.AssemblyInformationalVersion = {ThisAssembly.AssemblyInformationalVersion}");
+        Console.WriteLine($"ThisAssembly.AssemblyFileVersion = {ThisAssembly.AssemblyFileVersion}");
+        Console.WriteLine($"ThisAssembly.AssemblyVersion = {ThisAssembly.AssemblyVersion}");
         var v = ThisAssembly.AssemblyInformationalVersion;
         var r = Regex.Replace(v, "\\+([0-9a-zA-Z]+)$", "+g$1");
+        Console.WriteLine($"v = {v} ; r = {r}");
         return r;
     }
 
