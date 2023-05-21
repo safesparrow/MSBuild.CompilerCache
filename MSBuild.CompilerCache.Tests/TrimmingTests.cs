45f17cd87c2777fc80318483397e87e3c98a2983
janusz <janusz.wrobel.2@gmail.com>
2023-05-21 18:57:35 +0100

    WIP

--- MSBuild.CompilerCache.Tests/TrimmingTests.cs
+++ MSBuild.CompilerCache.Tests/TrimmingTests.cs
@@ -21,24 +21,6 @@ public class TrimmingTests
     }
 }
 
-public class RefCache : IRefCache
-{
-    public bool Exists(CacheKey key)
-    {
-        throw new NotImplementedException();
-    }
-
-    public RefDataWithOriginalExtract? Get(CacheKey key)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void Set(CacheKey key, RefDataWithOriginalExtract data)
-    {
-        throw new NotImplementedException();
-    }
-}
-
 [TestFixture]
 public class CachedTrimmingTests
 {
@@ -54,4 +36,57 @@ public class CachedTrimmingTests
             .Select(filepath => Locator.GetAllRefData(filepath, cache))
             .ToImmutableArray();
     }
+}
+
+[TestFixture]
+public class RefCacheTests
+{
+    [Test]
+    public void EmptyCacheMisses()
+    {
+        using var dir = new DisposableDir();
+        var cache = new RefCache(dir.FullName);
+        var key = new CacheKey("a");
+        Assert.Multiple(() =>
+        {
+            Assert.That(cache.Exists(key), Is.False);
+            Assert.That(cache.Get(key), Is.Null);
+        });
+    }
+
+    [Test]
+    public void AfterSetCacheHits()
+    {
+        using var dir = new DisposableDir();
+        var cache = new RefCache(dir.FullName);
+        var key = new CacheKey("a");
+        var data = new RefDataWithOriginalExtract(
+            Ref: new RefData(
+                PublicRefHash: "public",
+                PublicAndInternalRefHash: "publicandinternal",
+                InternalsVisibleTo: ImmutableArray.Create("asm1")
+            ),
+            Original: new LocalFileExtract(
+                Path: "a",
+                Hash: "original_hash",
+                Length: 1212,
+                LastWriteTimeUtc: DateTime.MaxValue
+            )
+        );
+        cache.Set(key, data);
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(cache.Exists(key), Is.True);
+            var cached = cache.Get(key);
+            Assert.That(cached, Is.Not.Null);
+            Assert.That(cached.ToJson(), Is.EqualTo(data.ToJson()));
+            Assert.That(cache.Exists(new CacheKey("b")), Is.False);
+        }); 
+    }
+}
+
+public static class Extensions
+{
+    public static string ToJson(this object x) => Newtonsoft.Json.JsonConvert.SerializeObject(x);
 }
---
+++ MSBuild.CompilerCache/RefCache.cs
@@ -0,0 +1,49 @@
+namespace MSBuild.CompilerCache;
+
+public class RefCache : IRefCache
+{
+    private readonly string _cacheDir;
+
+    public RefCache(string cacheDir)
+    {
+        _cacheDir = cacheDir;
+        Directory.CreateDirectory(_cacheDir);
+    }
+
+    public string EntryPath(CacheKey key) => Path.Combine(_cacheDir, $"{key.Key}.json");
+    
+    public bool Exists(CacheKey key)
+    {
+        var entryPath = EntryPath(key);
+        return File.Exists(entryPath);
+    }
+
+    public RefDataWithOriginalExtract? Get(CacheKey key)
+    {
+        var entryPath = EntryPath(key);
+        if (File.Exists(entryPath))
+        {
+            var json = File.ReadAllText(entryPath);
+            var data = Newtonsoft.Json.JsonConvert.DeserializeObject<RefDataWithOriginalExtract>(json);
+            return data;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public void Set(CacheKey key, RefDataWithOriginalExtract data)
+    {
+        var entryPath = EntryPath(key);
+        if (File.Exists(entryPath))
+        {
+            return;
+        }
+        else
+        {
+            var json = Newtonsoft.Json.JsonConvert.SerializeObject(data);
+            File.WriteAllText(entryPath, json);
+        }
+    }
+}
